83fd8a3d23ba5360cd35623e82a054738067c503
Aman Tripathi <amanttripathi02@gmail.com>
2025-11-08 23:28:06 +0530

    Initiated Search input

--- gothamstore/src/app/(app)/(home)/search-filters/index.tsx
+++ gothamstore/src/app/(app)/(home)/search-filters/index.tsx
@@ -1,3 +1,6 @@
+import SearchInput from "./search-input";
+
+
 interface Props{
 
     data: any;
@@ -5,7 +8,8 @@ interface Props{
 
 export const SearchFilters = ({ data }: Props) => {
   return (
-    <div>
+    <div className="px-4 lg:px-4 py-8 border-b flex flex-col gap-4 w-full">
+      <SearchInput />
         <h2>Search Filters Component</h2>
         {JSON.stringify(data,null,2)}
     </div>
